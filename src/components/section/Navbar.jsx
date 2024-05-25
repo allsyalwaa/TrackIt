c6c4826c1a437bb0692c0caad457a31404c09bd6
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-25 21:05:34 +0700

    Set up Project TrackIt

---
+++ src/components/section/Navbar.jsx
@@ -0,0 +1,11 @@
+import Logo from "../../assets/logo.svg";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <img src={Logo} alt="" />
+      </div>
+    </nav>
+  );
+}
