74154be6abaacae76cd55cc4e0eba09c9dca7b25
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-27 20:40:00 +0700

    refactor: Remove unused images and update MainLayout component

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,5 @@
 import Navbar from "../components/section/Navbar";
-import Logo from "../assets/illustration-logo.svg";
-import Image1 from "../assets/illustration-task.svg";
-import Image2 from "../assets/illustration-dashboard.svg";
 import Footer from "../components/section/Footer";
-import { NavLink } from "react-router-dom";
 
 export default function MainLayout({ children }) {
   return (
