81908a3ddcc71b83d6126b207d4b4375178fd4a0
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-28 20:29:37 +0700

    Create a responsive SecHero component

--- src/components/section/SecHero.jsx
+++ src/components/section/SecHero.jsx
@@ -6,22 +6,28 @@ export default function SecHero() {
   return (
     <section className="container my-12">
       <div className="flex flex-col items-center ">
-        <div className="flex items-center justify-center gap-10 text-secondary">
-          <h1 className="text-6xl font-bold">Welcome to</h1>
-          <img className="h-24 " src={Logo} alt="TrackIt" />
+        <div className="flex flex-col content-center items-center gap-10 text-secondary md:flex-row md:justify-center">
+          <h1 className="text-center text-6xl font-bold">Welcome to</h1>
+          <img className="h-16 md:h-24 " src={Logo} alt="TrackIt" />
         </div>
 
-        <p className="mt-6 w-3/5 text-center text-xl font-medium text-black/50">
+        <p className="mt-6 text-center text-lg font-medium text-black/50 md:w-3/5 md:text-xl">
           TrackIt is your all-in-one web solution for effortlessly managing all
           your daily activities. Stay organized and productive with TrackIt -
           Everything in One Place!
         </p>
       </div>
 
-      <div className="mx-auto mt-8 grid max-w-screen-md grid-cols-2 gap-36">
-        <Signup />
-        <div>
-          <img className="w-full" src={Image1} alt="illustration-task" />
+      <div className="mx-auto mt-8 grid max-w-screen-md gap-12 md:grid-cols-2 md:gap-36">
+        <div className="order-2 mx-auto w-2/3 md:order-1 md:w-full">
+          <Signup />
+        </div>
+        <div className="order-1 flex justify-center md:order-2">
+          <img
+            className="w-1/2 md:w-full"
+            src={Image1}
+            alt="illustration-task"
+          />
         </div>
       </div>
     </section>
