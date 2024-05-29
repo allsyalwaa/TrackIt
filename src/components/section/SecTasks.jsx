d693a483f34da7965137b0a34fe7c771a7a5dbdd
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-29 23:18:01 +0700

    Create AddTasks feature for SecTasks component

--- src/components/section/SecTasks.jsx
+++ src/components/section/SecTasks.jsx
@@ -1,7 +1,20 @@
+import AddTask from "../ui/AddTask";
 import ButtonPlus from "../ui/ButtonPlus";
 import CardTasks from "../ui/CardTasks";
 
+import { useState } from "react";
+
 export default function SecTasks() {
+  const [isTaskPopupOpen, setIsTaskPopupOpen] = useState(false);
+
+  const handleOpenTaskPopup = () => {
+    setIsTaskPopupOpen(true);
+  };
+
+  const handleCloseTaskPopup = () => {
+    setIsTaskPopupOpen(false);
+  };
+
   return (
     <section>
       <div className="text-3xl font-bold text-secondary">My Tasks</div>
@@ -12,9 +25,14 @@ export default function SecTasks() {
         <CardTasks text1="Task name" text2="Description" />
         <CardTasks text1="Task name" text2="Description" />
       </div>
-      <div className="absolute bottom-8 right-8">
+      <button
+        onClick={handleOpenTaskPopup}
+        className="absolute bottom-8 right-8"
+      >
         <ButtonPlus />
-      </div>
+      </button>
+
+      {isTaskPopupOpen && <AddTask onClose={handleCloseTaskPopup} />}
     </section>
   );
 }
