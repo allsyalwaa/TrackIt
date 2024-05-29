46d6dd08c8d3b6f35e79f7545429d7c3644276de
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-05-29 17:34:29 +0700

    Update SecAlarm component to improve accessibility and responsiveness

--- src/components/section/SecAlarm.jsx
+++ src/components/section/SecAlarm.jsx
@@ -2,15 +2,17 @@ export default function SecAlarm() {
   return (
     <section>
       <div className=" flex w-full flex-col items-center justify-center gap-6">
-        <h1 className="text-2xl font-bold text-primary">
+        <h1 className="text-xl font-bold text-primary md:text-2xl">
           Thursday, May 23, 2024
         </h1>
-        <h1 className="text-7xl font-bold text-secondary">7:57:54 PM</h1>
+        <h1 className="text-5xl font-bold text-secondary md:text-7xl">
+          7:57:54 PM
+        </h1>
       </div>
 
       <form className="mt-6" action="">
         <h2 className="text-lg font-medium text-primary">Set alarm time</h2>
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid gap-3 md:grid-cols-2 md:gap-6">
           <div>
             <label className="text-sm text-primary" htmlFor="hours">
               Hours
@@ -86,10 +88,16 @@ export default function SecAlarm() {
       </form>
       <hr className="mt-6 border-t border-primary/50" />
 
-      <div className="mx-auto flex justify-between rounded-lg border-[1.5px] border-primary px-4 py-3">
-        <h1 className="text-lg font-semibold text-primary">Alarm name</h1>
-        <h1 className="text-3xl font-bold text-secondary">06.00 PM</h1>
-        <h1 className="text-lg font-semibold text-primary">Friday, May 24</h1>
+      <div className="mx-auto flex items-center justify-between rounded-lg border-[1.5px] border-primary px-4 py-3 text-center">
+        <h1 className="text-sm font-semibold text-primary md:text-lg">
+          Alarm name
+        </h1>
+        <h1 className="text-xl font-bold text-secondary md:text-3xl">
+          06.00 PM
+        </h1>
+        <h1 className="text-sm font-semibold text-primary md:text-lg">
+          Friday, May 24
+        </h1>
       </div>
     </section>
   );
