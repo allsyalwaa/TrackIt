2b376afa6d5cad0d8852ced85db0883a9cba75e9
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-14 21:44:34 +0700

    refactor: Update CardTransactions component layout for better responsiveness

--- src/components/ui/CardTransactions.jsx
+++ src/components/ui/CardTransactions.jsx
@@ -1,7 +1,7 @@
 export default function CardTransactions({ text1, text2, money }) {
   return (
     <div className="rounded-lg border-[1.5px] border-primary/50 p-2">
-      <div className="mx-auto  grid grid-cols-2 items-center md:justify-between">
+      <div className="mx-auto  flex items-center justify-between">
         <div className="flex flex-col gap-1 md:gap-0">
           <h1 className="text-sm font-medium text-primary">{text1}</h1>
           <p className="text-xs font-medium text-primary/50 md:mt-2">{text2}</p>
