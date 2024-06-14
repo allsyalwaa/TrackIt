e1c5a79f9822aed7b09a8de5b01675adf261fc7a
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-14 21:43:59 +0700

    refactor: Update CardFinancial component layout for better responsiveness

--- src/components/ui/CardFinancial.jsx
+++ src/components/ui/CardFinancial.jsx
@@ -1,6 +1,6 @@
 export default function CardFinancial({ text, money }) {
   return (
-    <div className=" md:text-normal mx-auto mt-2 grid grid-cols-2 text-sm">
+    <div className=" md:text-normal mx-auto mt-2 flex justify-between text-sm">
       <h1 className="font-medium text-primary/50 ">{text} :</h1>
 
       <p className="flex justify-end font-medium text-primary">Rp {money},00</p>
