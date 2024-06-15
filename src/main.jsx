ef489361fa1bac19a1d785116f355abd75664ee5
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-15 19:16:46 +0700

    refactor: Update main.jsx to include GoogleOAuthProvider for Google sign-in functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId="701553922943-t7kh9h2ignf92r25qf8o7gchv4sptfo4.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
+);
