fe1dd08a975465f94373c0e30475f6b8dbae3d17
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-15 22:03:06 +0700

    feat: Update CardFinancial component to support different types of financial data

--- src/components/ui/CardFinancial.jsx
+++ src/components/ui/CardFinancial.jsx
@@ -1,9 +1,17 @@
-export default function CardFinancial({ text, money }) {
+import { rupiahFormat } from "../../utils";
+export default function CardFinancial({ text, money, type = "income" }) {
   return (
     <div className=" md:text-normal mx-auto mt-2 flex justify-between text-sm">
       <h1 className="font-medium text-primary/50 ">{text} :</h1>
 
-      <p className="flex justify-end font-medium text-primary">Rp {money},00</p>
+      <p
+        className={`flex justify-end font-medium  
+          ${type === "income" && "text-primary"}  
+          ${type === "expenditure" && "text-[#EE0C00]"}
+          `}
+      >
+        {rupiahFormat(money)}
+      </p>
     </div>
   );
 }
