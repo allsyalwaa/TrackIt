275ad726d970988942a1ce419eb9fe2807aca9f1
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-15 23:24:38 +0700

    refactor: Update Sidebar component to fix visibility issue and improve styling

--- src/components/section/Sidebar.jsx
+++ src/components/section/Sidebar.jsx
@@ -35,7 +35,7 @@ export default function Sidebar() {
 
   return (
     <>
-      <div className="relative w-12 flex-shrink-0"></div>
+      <div className="relative w-12 flex-shrink-0 md:w-0"></div>
       <nav
         ref={sidebarRef}
         className={`fixed z-20 bg-primary py-6 transition-all duration-500 md:relative md:px-10 ${
