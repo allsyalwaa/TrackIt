aca90054051e5ec60161edf93c5dc233addea8d8
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-29 21:35:13 +0700

    refactor: Add BreadCrumbs component and update DashboardLayout to include it

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,4 +1,5 @@
 import Sidebar from "../components/section/Sidebar";
+import BreadCrumbs from "../components/ui/BreadCrumbs";
 
 export default function DashboardLayout({ children }) {
   return (
@@ -6,6 +7,7 @@ export default function DashboardLayout({ children }) {
       <div className="flex h-dvh ">
         <Sidebar />
         <main className="container mx-auto flex w-full flex-col p-8 ">
+          <BreadCrumbs />
           {children}
         </main>
       </div>
