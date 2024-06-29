fdefc29628143b1c93419345d89dbe2719d57618
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-29 20:05:40 +0700

    refactor: Update DashboardLayout component to use h-dvh class for height

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../components/section/Sidebar";
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <div className="flex h-screen ">
+      <div className="flex h-dvh ">
         <Sidebar />
         <main className="container mx-auto flex w-full flex-col overflow-y-auto p-8 ">
           {children}
