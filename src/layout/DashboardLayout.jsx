fe91e779517fc2567773c09e14c7cc09e81e5b5f
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-06-29 22:26:55 +0700

    refactor: Update DashboardLayout component to include data prop in BreadCrumbs

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,13 +1,13 @@
 import Sidebar from "../components/section/Sidebar";
 import BreadCrumbs from "../components/ui/BreadCrumbs";
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({ children, data }) {
   return (
     <>
-      <div className="flex h-dvh ">
+      <div className="flex min-h-dvh ">
         <Sidebar />
         <main className="container mx-auto flex w-full flex-col p-8 ">
-          <BreadCrumbs />
+          <BreadCrumbs data={data} />
           {children}
         </main>
       </div>
