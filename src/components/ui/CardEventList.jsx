f398540f0676b4c9432f89968a365cdc10b827b3
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-01 18:47:55 +0700

    feat: Add CardEventList component for displaying event cards

---
+++ src/components/ui/CardEventList.jsx
@@ -0,0 +1,14 @@
+export default function CardEventList({ text, time }) {
+  return (
+    <div className="mt-3">
+      <button className="flex w-full items-center justify-between">
+        <div className="h-2 w-2 rounded-full bg-primary"></div>
+        <p className="text-md ml-2 mr-auto font-semibold text-secondary">
+          {text}
+        </p>
+
+        <p className="text-sm text-primary/50">{time}</p>
+      </button>
+    </div>
+  );
+}
