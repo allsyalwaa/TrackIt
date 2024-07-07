61512e1cb082f471b81a4abfb2c30567cb75045f
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-07 23:05:58 +0700

    refactor: Update CardAlarm component layout for improved readability

--- src/components/ui/CardAlarm.jsx
+++ src/components/ui/CardAlarm.jsx
@@ -1,9 +1,15 @@
 export default function CardAlarm({ text, time, date }) {
   return (
-    <div className="mx-auto mt-4 flex items-center justify-between rounded-lg border-[1.5px] border-primary px-4 py-3 text-center">
-      <h1 className="text-sm font-semibold text-primary md:text-lg">{text}</h1>
-      <h1 className="text-xl font-bold text-secondary md:text-3xl">{time}</h1>
-      <h1 className="text-sm font-semibold text-primary md:text-lg">{date}</h1>
+    <div className="mx-auto mt-4 grid grid-cols-3 items-center rounded-lg border-[1.5px] border-primary px-4 py-3 ">
+      <h1 className="text-start text-sm font-semibold text-primary md:text-lg">
+        {text}
+      </h1>
+      <h1 className="text-center text-xl font-bold text-secondary md:text-3xl">
+        {time}
+      </h1>
+      <h1 className="text-end text-sm font-semibold text-primary md:text-lg">
+        {date}
+      </h1>
     </div>
   );
 }
