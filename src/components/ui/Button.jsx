47ec3a6ea789ba1b0fa12f84e067a23dc0696477
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-08 21:48:59 +0700

    refactor: Update Button component to include type prop for better flexibility

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,10 +4,12 @@ export default function Button({
   variant = "primary",
   className,
   onClick,
+  type,
 }) {
   return (
     <button
       onClick={onClick}
+      type={type}
       className={`
         flex items-center justify-center gap-4 rounded-full  border-[1.5px] px-4 py-1 font-medium sm:px-5 sm:py-2
         ${variant === "secondary" && "border-secondary bg-secondary text-sm text-white"}
