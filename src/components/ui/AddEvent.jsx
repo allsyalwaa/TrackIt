0e6e50b8279213e64457dd482a14aa9ddcd423fb
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-08 21:48:52 +0700

    refactor: Update SecCalendar and EventList components to fetch events dynamically

--- src/components/ui/AddEvent.jsx
+++ src/components/ui/AddEvent.jsx
@@ -1,13 +1,102 @@
 import { useState } from "react";
 import Button from "./Button";
+import { getEvents } from "../../utils/FetchData";
 
-export default function AddEvent({ onClose }) {
+export default function AddEvent({ onClose, selectedDate, onEventAdded }) {
   const [isOpen, setIsOpen] = useState(true);
 
   const closePopup = () => {
     setIsOpen(false);
     onClose();
   };
+
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+
+  const handleHoursInput = (event) => {
+    let value = event.target.value;
+    value = value.replace(/[^0-9]/g, ""); // Hanya angka
+    if (value !== "" && (parseInt(value) < 1 || parseInt(value) > 12)) {
+      value = value.slice(0, -1); // Hapus karakter terakhir jika tidak valid
+    }
+    event.target.value = value;
+  };
+
+  const handleMinutesInput = (event) => {
+    let value = event.target.value;
+    value = value.replace(/[^0-9]/g, ""); // Hanya angka
+    if (value.length > 2) {
+      value = value.slice(0, 2); // Batasi panjang input maksimal 2 karakter
+    }
+    if (value !== "" && parseInt(value) > 59) {
+      value = value.slice(0, -1); // Hapus karakter terakhir jika tidak valid
+    }
+    event.target.value = value;
+  };
+
+  const BASE_URL = import.meta.env.VITE_API_URL;
+
+  const postEvents = async (events) => {
+    try {
+      const response = await fetch(BASE_URL + "/calendar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(events),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      console.log("Success:", data);
+      onEventAdded();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    let hours = form.hours.value;
+    let minutes = form.minutes.value;
+    const title = form.title.value;
+    const time = form.time.value;
+
+    // Tambahkan 0 di depan jika panjang input 1
+    if (hours.length === 1) {
+      hours = "0" + hours;
+    }
+    if (minutes.length === 1) {
+      minutes = "0" + minutes;
+    }
+
+    console.log(title, hours, minutes, time);
+    const date = `${selectedDate}T${hours}:${minutes}:00${time}.000Z`;
+
+    console.log(date);
+
+    const newEvent = {
+      title: title,
+      start: `${selectedDate}T${hours}:${minutes}:00${time}.000Z`,
+      end: `${selectedDate}T${hours}:${minutes}:00${time}.000Z`,
+    };
+
+    postEvents(newEvent);
+    closePopup();
+  };
+
   return (
     <>
       {isOpen && (
@@ -30,9 +119,12 @@ export default function AddEvent({ onClose }) {
             </div>
 
             <p className="text-sm font-bold text-primary/50">
-              Thursday, 2 May 2024
+              {formatDate(selectedDate)}
             </p>
-            <form className="mt-4 flex w-full flex-col justify-start" action="">
+            <form
+              className="mt-4 flex w-full flex-col justify-start"
+              onSubmit={handleSubmit}
+            >
               <input
                 type="text"
                 id="title"
@@ -42,77 +134,66 @@ export default function AddEvent({ onClose }) {
               />
               <hr className="border-t-1 mt-2 w-full border-secondary" />
 
-              <div className="relative mt-4 grid grid-cols-3 items-center justify-between gap-6">
+              <div className="relative mt-4 grid grid-cols-3 items-center justify-between">
                 <label className="text-lg text-primary" htmlFor="hours">
                   Time :
                 </label>
+                <div className="col-span-2 grid grid-cols-3">
+                  <input
+                    type="text"
+                    placeholder="12"
+                    className="mx-auto w-full text-lg font-medium text-secondary placeholder-secondary/50 outline-none placeholder:text-base placeholder:font-semibold"
+                    name="hours"
+                    id="hours"
+                    maxLength="2"
+                    pattern="[0-9]*"
+                    onInput={handleHoursInput}
+                  />
 
-                <select
-                  className="mx-auto w-full text-lg font-medium text-secondary"
-                  name="hours"
-                  id="hours"
-                >
-                  <option value="">6 PM</option>
-                  <option value="">7 PM</option>
-                  <option value="">8 PM</option>
-                  <option value="">9 PM</option>
-                  <option value="">10 PM</option>
-                  <option value="">11 PM</option>
-                  <option value="">12 PM</option>
-                  <option value="">1 PM</option>
-                  <option value="">2 PM</option>
-                  <option value="">3 PM</option>
-                  <option value="">4 PM</option>
-                  <option value="">5 PM</option>
-                  <option value="">6 AM</option>
-                  <option value="">7 AM</option>
-                  <option value="">8 AM</option>
-                  <option value="">9 AM</option>
-                  <option value="">10 AM</option>
-                  <option value="">11 AM</option>
-                  <option value="">12 AM</option>
-                  <option value="">1 AM</option>
-                  <option value="">2 AM</option>
-                  <option value="">3 AM</option>
-                  <option value="">4 AM</option>
-                  <option value="">5 AM</option>
-                </select>
+                  <input
+                    type="text"
+                    placeholder="00"
+                    className="mx-auto w-full text-lg font-medium text-secondary placeholder-secondary/50 outline-none placeholder:text-base placeholder:font-semibold"
+                    name="minutes"
+                    id="minutes"
+                    maxLength="2"
+                    pattern="[0-9]*"
+                    onInput={handleMinutesInput}
+                  />
 
-                <select
-                  className="mx-auto w-full text-lg font-medium text-secondary"
-                  name="minutes"
-                  id="minutes"
-                >
-                  <option value="">00</option>
-                  <option value="">05</option>
-                  <option value="">10</option>
-                  <option value="">15</option>
-                  <option value="">20</option>
-                  <option value="">25</option>
-                  <option value="">30</option>
-                  <option value="">35</option>
-                  <option value="">40</option>
-                  <option value="">45</option>
-                  <option value="">50</option>
-                  <option value="">55</option>
-                </select>
+                  <select
+                    className="mx-auto w-full text-lg font-medium text-secondary"
+                    name="time"
+                    id="time"
+                  >
+                    <option value="">AM</option>
+                    <option value="">PM</option>
+                  </select>
+                </div>
               </div>
 
-              <div className="mt-4 grid grid-cols-2 justify-between">
+              {/* <div className="mt-4 grid grid-cols-3 justify-between">
                 <label className="text-lg text-primary">Location :</label>
                 <input
                   type="text"
-                  className="mr-0  border-none placeholder-secondary/50 outline-none placeholder:text-lg placeholder:font-semibold"
+                  className="col-span-2  mr-0 border-none placeholder-secondary/50 outline-none placeholder:text-base placeholder:font-semibold"
                   placeholder="Location"
                 />
               </div>
-              <div className="mt-4 grid grid-cols-2 justify-between">
+              <div className="mt-4 grid grid-cols-3 justify-between">
                 <label className="text-lg text-primary">Description :</label>
-                <input type="text" className="border-none  outline-none " />
-              </div>
+                <textarea
+                  className="col-span-2 border-none placeholder-secondary/50 outline-none placeholder:text-base placeholder:font-semibold"
+                  placeholder="Description"
+                />
+              </div> */}
 
               <div className="mt-6 flex justify-end">
-                <Button className={"md:w-1/4"} variant="primary">
+                <Button
+                  className={"md:w-1/4"}
+                  variant="primary"
+                  type={"submit"}
+                >
                   Save
                 </Button>
               </div>
