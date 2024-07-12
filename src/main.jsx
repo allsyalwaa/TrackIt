91518c40777afca5eada0274cc29bac078285519
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-12 20:59:47 +0700

    refactor: Add UserProvider to main.jsx for managing user context

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { UserProvider } from "./utils/UserContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="701553922943-t7kh9h2ignf92r25qf8o7gchv4sptfo4.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>,
+  <UserProvider>
+    <GoogleOAuthProvider clientId="701553922943-t7kh9h2ignf92r25qf8o7gchv4sptfo4.apps.googleusercontent.com">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GoogleOAuthProvider>
+  </UserProvider>,
 );
