af20749434a7a2f8815e393bee53f7c7e245644a
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-15 11:32:41 +0700

    refactor: Remove unnecessary code and comments in TimeInputHandlers.jsx

--- src/utils/TimeInputHandlers.jsx
+++ src/utils/TimeInputHandlers.jsx
@@ -1,20 +1,20 @@
 export const handleHoursInput = (event) => {
   let value = event.target.value;
-  value = value.replace(/[^0-9]/g, ""); // Hanya angka
+  value = value.replace(/[^0-9]/g, ""); 
   if (value !== "" && (parseInt(value) < 1 || parseInt(value) > 12)) {
-    value = value.slice(0, -1); // Hapus karakter terakhir jika tidak valid
+    value = value.slice(0, -1); 
   }
   event.target.value = value;
 };
 
 export const handleMinutesInput = (event) => {
   let value = event.target.value;
-  value = value.replace(/[^0-9]/g, ""); // Hanya angka
+  value = value.replace(/[^0-9]/g, ""); 
   if (value.length > 2) {
-    value = value.slice(0, 2); // Batasi panjang input maksimal 2 karakter
+    value = value.slice(0, 2); 
   }
   if (value !== "" && parseInt(value) > 59) {
-    value = value.slice(0, -1); // Hapus karakter terakhir jika tidak valid
+    value = value.slice(0, -1); 
   }
   event.target.value = value;
 };
