ac4fce583fe72eb6bccf8f240088bbfac6216758
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-15 11:23:25 +0700

    refactor: Update components to use service methods and remove unused code

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 import ButtonGoogle from "../components/ui/ButtonGoogle";
 import Button from "../components/ui/Button";
 import { useUserContext } from "../utils/UserContext";
+import { validatePassword, handleSignup } from "../utils/fetchdata/SignUpService";
 
 export default function SignUp() {
   const BASE_URL = import.meta.env.VITE_API_URL;
@@ -23,15 +24,6 @@ export default function SignUp() {
     flow: "auth-code",
   });
 
-  const validateEmail = (email) => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-  };
-
-  const validatePassword = (password) => {
-    return password.length >= 8;
-  };
-
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
@@ -42,144 +34,122 @@ export default function SignUp() {
     }
   };
 
-  const handleSignup = async (e) => {
-    e.preventDefault();
-    if (!validateEmail(email)) {
-      setError("Invalid email address");
-      return;
-    }
-    if (!validatePassword(password)) {
-      setPasswordError("Password must be at least 8 characters long");
-      return;
-    }
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-
-    const response = await fetch(BASE_URL + "/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setUser(data);
-      localStorage.setItem("userId", 2);
-      navigate("/dashboard");
-    } else {
-      const result = await response.json();
-      setError(result.message || "An error occurred");
-    }
+  const handleFormSubmit = (e) => {
+    handleSignup(
+      e,
+      email,
+      password,
+      confirmPassword,
+      setUser,
+      setError,
+      setPasswordError,
+      navigate,
+      BASE_URL,
+    );
   };
 
   return (
-    <>
-      <div className="container py-16">
-        <div className="flex flex-col items-center justify-center gap-6">
-          <img className="h-20" src={Logo} alt="" />
+    <div className="container py-16">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <img className="h-20" src={Logo} alt="Logo" />
 
-          <h1 className="text-center text-3xl font-bold text-secondary">
-            Check out TrackIt — it&apos;s free!
-          </h1>
+        <h1 className="text-center text-3xl font-bold text-secondary">
+          Check out TrackIt — it&apos;s free!
+        </h1>
 
-          <p className="w-2/3 text-center text-lg font-medium text-black/50">
-            TrackIt helps individuals, professionals, and small businesses
-            around the world simplify their daily activities and stay organized.
-          </p>
+        <p className="w-2/3 text-center text-lg font-medium text-black/50">
+          TrackIt helps individuals, professionals, and small businesses around
+          the world simplify their daily activities and stay organized.
+        </p>
 
-          <div>
-            <ButtonGoogle onClick={() => login()}>
-              Sign up with Google
-            </ButtonGoogle>
+        <div>
+          <ButtonGoogle onClick={() => login()}>
+            Sign up with Google
+          </ButtonGoogle>
 
-            <div className="mt-4 flex items-center px-4">
-              <div className="flex-grow border-t border-gray-400"></div>
-              <span className="px-4 text-gray-500">or</span>
-              <div className="flex-grow border-t border-gray-400"></div>
-            </div>
+          <div className="mt-4 flex items-center px-4">
+            <div className="flex-grow border-t border-gray-400"></div>
+            <span className="px-4 text-gray-500">or</span>
+            <div className="flex-grow border-t border-gray-400"></div>
+          </div>
 
-            <form className="flex flex-col" onSubmit={handleSignup}>
-              <input
-                className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <input
-                className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
-                type="password"
-                placeholder="Create a password"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-              />
+          <form className="flex flex-col" onSubmit={handleFormSubmit}>
+            <input
+              className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
+              type="password"
+              placeholder="Create a password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
 
-              {passwordError && (
-                <p className="mt-2 text-xs text-red-500">{passwordError}</p>
-              )}
+            {passwordError && (
+              <p className="mt-2 text-xs text-red-500">{passwordError}</p>
+            )}
 
-              <input
-                className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
-                type="password"
-                placeholder="Confirm a password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
+            <input
+              className="mt-4 rounded-full border-[1.5px] border-primary/50 px-5 py-3 text-xs placeholder-primary/50 sm:text-base"
+              type="password"
+              placeholder="Confirm a password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
 
-              {error && error === "Passwords do not match" && (
-                <p className="mt-2 text-xs text-red-500">{error}</p>
-              )}
+            {error && error === "Passwords do not match" && (
+              <p className="mt-2 text-xs text-red-500">{error}</p>
+            )}
 
-              <Button
-                variant="primary"
-                className={"mt-6 w-full px-5 py-3"}
-                type="submit"
-              >
-                Sign Up
-              </Button>
+            <Button
+              variant="primary"
+              className={"mt-6 w-full px-5 py-3"}
+              type="submit"
+            >
+              Sign Up
+            </Button>
 
-              <p className="mt-2 text-xs font-medium text-black/50">
-                By signing up, you are indicating that you have read and agree
-                to the{" "}
-                <span className="text-primary">
-                  <NavLink
-                    to="/coming-soon"
-                    className="text-xs font-medium text-primary"
-                  >
-                    Terms of Use
-                  </NavLink>
-                </span>{" "}
-                and{" "}
-                <span className="text-primary">
-                  <NavLink
-                    to="/coming-soon"
-                    className="text-xs font-medium text-primary"
-                  >
-                    Privacy Policy
-                  </NavLink>
-                </span>
-                .
-              </p>
-            </form>
+            <p className="mt-2 text-xs font-medium text-black/50">
+              By signing up, you are indicating that you have read and agree to
+              the{" "}
+              <span className="text-primary">
+                <NavLink
+                  to="/coming-soon"
+                  className="text-xs font-medium text-primary"
+                >
+                  Terms of Use
+                </NavLink>
+              </span>{" "}
+              and{" "}
+              <span className="text-primary">
+                <NavLink
+                  to="/coming-soon"
+                  className="text-xs font-medium text-primary"
+                >
+                  Privacy Policy
+                </NavLink>
+              </span>
+              .
+            </p>
+          </form>
 
-            <div className="mt-6 flex items-center justify-center gap-1">
-              <p className="text-xs font-medium text-black/50">
-                Already have an account?
-              </p>
-              <NavLink to="/login" className="text-xs font-medium text-primary">
-                Log In
-              </NavLink>
-            </div>
+          <div className="mt-6 flex items-center justify-center gap-1">
+            <p className="text-xs font-medium text-black/50">
+              Already have an account?
+            </p>
+            <NavLink to="/login" className="text-xs font-medium text-primary">
+              Log In
+            </NavLink>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
