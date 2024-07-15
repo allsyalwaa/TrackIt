782ce773d1192044b61e6a50ff47fed2ae0eeb61
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-15 12:11:49 +0700

    feat: Add CardTransactions component for displaying transaction information in a card

---
+++ src/components/ui/cardcontent/CardTransactions.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import EditTransaction from "../editcontent/EditTransaction";
+import ConfirmDelete from "../ConfirmDelete";
+import { rupiahFormat } from "../../../utils";
+import { deleteTransaction } from "../../../utils/fetchdata/MoneyCalculatorService";
+
+export default function CardTransactions({
+  text1,
+  text2,
+  money,
+  transactionId,
+  onDelete,
+}) {
+  const [isTransactionPopupOpen, setIsTransactionPopupOpen] = useState(false);
+  const [isConfirmDeletePopupOpen, setIsConfirmDeletePopupOpen] =
+    useState(false);
+
+  const handleOpenTransactionPopup = () => {
+    setIsTransactionPopupOpen(true);
+  };
+
+  const handleCloseTransactionPopup = () => {
+    setIsTransactionPopupOpen(false);
+  };
+
+  const handleOpenConfirmDeletePopup = () => {
+    setIsConfirmDeletePopupOpen(true);
+  };
+
+  const handleCloseConfirmDeletePopup = () => {
+    setIsConfirmDeletePopupOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteTransaction(transactionId);
+      onDelete(transactionId);
+      setIsConfirmDeletePopupOpen(false);
+    } catch (error) {
+      console.error("Error deleting transaction:", error);
+    }
+  };
+
+  const moneyColor = money < 0 ? "text-[#EE0C00]" : "text-primary";
+
+  return (
+    <div className="rounded-lg border-[1.5px] border-primary/50 p-2">
+      <div className="mx-auto flex items-center justify-between">
+        <div className="flex flex-col gap-1 md:gap-0">
+          <h1 className="text-sm font-medium text-primary">{text1}</h1>
+          <p className="text-xs font-medium text-primary/50 md:mt-2">{text2}</p>
+          <h1 className={`flex font-medium md:hidden ${moneyColor}`}>
+            {rupiahFormat(money)}
+          </h1>
+        </div>
+        <div className="flex justify-end gap-3">
+          <h1
+            className={`hidden justify-end font-medium md:flex ${moneyColor}`}
+          >
+            {rupiahFormat(money)}
+          </h1>
+          <button onClick={handleOpenTransactionPopup} className="text-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.3em"
+              height="1.3em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="m14.06 9l.94.94L5.92 19H5v-.92zm3.6-6c-.25 0-.51.1-.7.29l-1.83 1.83l3.75 3.75l1.83-1.83c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94z"
+              />
+            </svg>
+          </button>
+          <button
+            onClick={handleOpenConfirmDeletePopup}
+            className="text-primary"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.3em"
+              height="1.3em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      {isTransactionPopupOpen && (
+        <EditTransaction onClose={handleCloseTransactionPopup} />
+      )}
+      {isConfirmDeletePopupOpen && (
+        <ConfirmDelete
+          onClose={handleCloseConfirmDeletePopup}
+          onDelete={handleDelete}
+        />
+      )}
+    </div>
+  );
+}
