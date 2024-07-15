867f5bdfb5b521864afb8ac13240371fcc5dcd00
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-15 11:24:20 +0700

    refactor: Add EventService module for handling event data fetching and posting

---
+++ src/utils/fetchdata/EventService.jsx
@@ -0,0 +1,30 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export async function getEvents(date) {
+  try {
+    const response = await fetch(
+      BASE_URL + `/calendar${date ? `?date=${date}` : ""}`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export const postEvent = async (event) => {
+  const response = await fetch(`${BASE_URL}/calendar`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(event),
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  return response.json();
+};
