eb70611f405936c38f46c8926aab090b551a3a19
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-15 12:11:21 +0700

    feat: Add CardDashboard component for displaying dashboard information in a card

---
+++ src/components/ui/cardcontent/CardDashboard.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+export default function CardDashboard({ link, text1, text2, image }) {
+  return (
+    <NavLink
+      to={link}
+      className="flex h-full flex-col items-center justify-center gap-3 rounded-lg border-[1.5px] border-primary p-3  hover:bg-primary/10 [&_h3]:hover:scale-110 [&_img]:hover:scale-110"
+    >
+      <h3 className="text-xl font-bold text-primary">{text1}</h3>
+      {<img className="h-20" src={image} alt="" />}
+      <p className="text-center text-sm font-medium text-primary/50">{text2}</p>
+    </NavLink>
+  );
+}
