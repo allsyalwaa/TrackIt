29a930cadf88a797efef92907781cc0187282abb
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-29 10:57:48 +0700

    feat: Update DashboardLayout to fetch user data from API

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,7 +1,33 @@
 import Sidebar from "../components/section/Sidebar";
 import BreadCrumbs from "../components/ui/BreadCrumbs";
+import { useEffect } from "react";
+import { useUserContext } from "../utils/UserContext";
 
 export default function DashboardLayout({ children, data }) {
+  const { setUser } = useUserContext();
+  const BASE_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    if (userId) {
+      fetch(BASE_URL + `/user/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setUser({
+            email: data.email,
+            username: data.username,
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+    } else {
+      setUser({
+        email: "syalwa@gmail.com",
+        username: "syalwa",
+      });
+    }
+  }, []);
   return (
     <>
       <div className="flex min-h-dvh ">
--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -3,34 +3,11 @@ import { createContext, useState, useContext, useEffect } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const BASE_URL = import.meta.env.VITE_API_URL;
+  // const BASE_URL = import.meta.env.VITE_API_URL;
   const [user, setUser] = useState({
-    email: "",
-    username: "",
+    email: "default@mail.com",
+    username: "default",
   });
-
-  useEffect(() => {
-    const userId = localStorage.getItem("userId");
-
-    if (userId) {
-      fetch(BASE_URL + `/user/${userId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setUser({
-            email: data.email,
-            username: data.username,
-          });
-        })
-        .catch((error) => {
-          console.error("Error fetching user data:", error);
-        });
-    } else {
-      setUser({
-        email: "syalwa@gmail.com",
-        username: "syalwa",
-      });
-    }
-  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
