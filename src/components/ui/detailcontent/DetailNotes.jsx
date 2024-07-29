d79360cf2da105a50d800bf24ef5778692c2a067
zulfiasyalwa4 <109894590+zulfiasyalwa4@users.noreply.github.com>
2024-07-29 11:08:54 +0700

    refactor: Update DetailNotes and DetailReminder components to fix SVG path

--- src/components/ui/detailcontent/DetailNotes.jsx
+++ src/components/ui/detailcontent/DetailNotes.jsx
@@ -60,7 +60,7 @@ export default function DetailNotes({ id, onClose }) {
                 >
                   <path
                     fill="currentColor"
-                    d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 1 0 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
+                    d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
                   />
                 </svg>
               </button>
--- src/components/ui/detailcontent/DetailReminder.jsx
+++ src/components/ui/detailcontent/DetailReminder.jsx
@@ -80,7 +80,7 @@ export default function DetailReminder({ id, onClose }) {
                 >
                   <path
                     fill="currentColor"
-                    d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 1 0 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
+                    d="m289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34Z"
                   />
                 </svg>
               </button>
